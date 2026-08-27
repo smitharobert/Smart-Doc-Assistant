a8630bec47a7c73c2748b056998c10163bc95bea
Smitha_Robert <smitharobert.works@gmail.com>
2026-08-27 16:33:44 +0400

    feat: complete Stage 1 & 2 DocumentProcessor

---
+++ app/core/document_processor.py
@@ -0,0 +1,32 @@
+import docx 
+from pypdf import PdfReader
+class DocumentProcessor:
+    extract_text_from_docx(self,file_path):
+    extract_text_from_pdf(self,file_path)
+    chunk_text(self,text,chunk_size=50,overlap=10)
+
+def extract_text_from_docx(file_path):
+    doc=docx.Document(file_path)
+    text_list=[]
+    for d in doc.paragraphs:
+        text_list.append(d.text)
+    return "\n".join(text_list) 
+
+def extract_text_from_pdf(file_path):
+    reader=PdfReader(file_path)
+    text_list=[]
+    for page in reader.pages:
+        text_list.append(page.extract_text())
+    return "\n".join(text_list)
+
+def chunk_text(text,chunk_size=50,overlap=10):
+    words=text.split()
+    stepsize=chunk_size-overlap
+    chunks=[]
+    start=0
+    while start <len(words):
+        chunk_words=words[start:start+chunk_size]
+        if len(chunk_words)>=15:
+            chunks.append(" ".join(chunk_words))
+        start+=stepsize
+    return chunks 
